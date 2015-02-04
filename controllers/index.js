db4d5453cfda1913274fc8b24851e6c27010c721
Eric Rucker <eric.rucker@yale.edu>
2015-02-04 14:55:04 +0000

    Views update

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@
  */
 function index (request, response) {
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'title': 'Agile Aardvarks',
+    'tagline': 'We can do it!'
   };
   response.render('index.html', contextData);
 }
